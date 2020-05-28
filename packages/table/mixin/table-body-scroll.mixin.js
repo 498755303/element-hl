837cbe421009ad6328ac52fe614d01e139150aa7
huliang@xm9m.com <huliang@xm9m.com>
2020-05-28 14:33:57 +0800

    elementui 2.13.2修改源码

---
+++ packages/table/mixin/table-body-scroll.mixin.js
@@ -0,0 +1,42 @@
+/*
+ * @Description: 增加表格滚动事件和方法
+ * @LastEditors: HL
+ * @LastEditTime: 2020-05-28 10:42:36
+ */
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    // 绑定表体滚动事件
+    this.$nextTick(() => {
+      const tableBodyWrapper = this.$el.querySelector('.scrolling_adesk');
+      tableBodyWrapper.addEventListener('scroll', this.handleScroll);
+      tableBodyWrapper.addEventListener('DOMMouseScroll', this.handleScroll);
+    });
+  },
+  methods: {
+    // 表体滚动到什么位置
+    pagingScrollTop(top, left) {
+      const tableBodyWrapper = this.$el.querySelector('.scrolling_adesk');
+      if (tableBodyWrapper) {
+        tableBodyWrapper.scrollTop = top || 0;
+        tableBodyWrapper.scrollLeft = left || 0;
+      }
+    },
+    handleScroll(e) {
+      // 解决 滚动时 行hover高亮的问题
+      this.store.states.hoverRow = null;
+      // 当表格滚动暴露滚动事件
+      this.$emit('tableBodyScroll', this, e);
+    }
+  },
+  beforeDestroy() {
+    // 解绑事件
+    const tableBodyWrapper = this.$el.querySelector('.scrolling_adesk');
+    if (tableBodyWrapper) {
+      tableBodyWrapper.removeEventListener('scroll', this.handleScroll);
+      tableBodyWrapper.removeEventListener('DOMMouseScroll', this.handleScroll);
+    }
+  }
+};
---
+++ version.md
@@ -0,0 +1,12 @@
+<!--
+ * @Description: 版本更新说明
+ * @LastEditors: HL
+ * @LastEditTime: 2020-05-28 10:59:33
+ -->
+
+### 2.13.201
+1. modify table scrollEvent 增加了一个表格的scroll对外事件tableBodyScroll 
+2. and add chinese2 for input by HL 增加了Input输入框的一个属性doubleCodeable,中文字符算两字节
+### 2.13.201
+1. table模块:增加了一个noCalcScrollX:控制是否计算横向滚动条的高度,用于生成fix header的高度
+2. table模块:增加了一个emptyHidden:用于控制无数据时是否隐藏表体
